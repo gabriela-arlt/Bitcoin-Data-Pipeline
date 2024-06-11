b7e8419a3f1ef9d2ad0b79bfdcbc47a9eb116ec7
gabriela-arlt <gabriela.arlt.mail@gmail.com>
2024-06-11 15:17:10 +0200

    color

--- connection.py
+++ connection.py
@@ -104,6 +104,15 @@ fig1 = px.line(bitcoin_prices_df, x='date', y=['low', 'high', 'close','open'], l
        'value': 'Price',
        'date': 'Date'
       }, title='DailyPrices for Bitcoin')
+# Customize the layout to change the background color
+fig1.update_layout(
+    xaxis_title='Date',
+    yaxis_title='Price',
+    legend_title='Price Type',
+    plot_bgcolor='#e0f7fa',  # Bright blue background for the plot area
+    paper_bgcolor='#e0f7fa',  # Bright blue background for the entire figure
+    title_font={'color': '#00008b'} # Dark blue color for the title
+  )
 
 # Display the Plotly chart
 st.plotly_chart(fig1, use_container_width=True)
