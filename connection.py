e4983fde14fbc406c11be8ece835fed45ed76638
gabriela-arlt <gabriela.arlt.mail@gmail.com>
2024-06-12 10:16:27 +0200

    color

--- connection.py
+++ connection.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 import plotly.graph_objects as go
 from plotly.colors import sample_colorscale
 
+# Set Streamlit layout to wide
+st.set_page_config(layout="wide")
+
 load_dotenv()
 
 # Function to connect to the Database
@@ -41,10 +44,8 @@ def fetch_bitcoin_news(engine):
 
 # Get the database connection
 conn = get_connection()
-# Set Streamlit layout to wide
-st.set_page_config(layout="wide")
 
-# Custom CSS to change background color to very light blue and font color to dark blue
+# Custom CSS to change background color to very light blue and set header colors
 st.markdown(
     """
     <style>
@@ -54,12 +55,12 @@ st.markdown(
     .centered-title {
         text-align: center;
         color: #00008b;
-        }
+    }
     h1 {
         color: orange;
     }
     h2 {
-        color: #00008b;
+        color: darkblue;
     }
     </style>
     """,
@@ -79,6 +80,9 @@ bitcoin_prices_df['date'] = pd.to_datetime(bitcoin_prices_df['date'])
 bitcoin_prices_df['month'] = bitcoin_prices_df['date'].dt.month
 bitcoin_prices_df['year'] = bitcoin_prices_df['date'].dt.year
 
+# Group by month and calculate mean of low, high, close, and open
+monthly_data = bitcoin_prices_df.groupby('month').agg({'low': 'mean', 'high': 'mean', 'close': 'mean', 'open':'mean'}).reset_index()
+
 # Create a mapping from month numbers to month names
 month_map = {
     1: 'January', 2: 'February', 3: 'March', 4: 'April',
@@ -87,12 +91,7 @@ month_map = {
 }
 
 # Apply the mapping to the 'month' column in monthly_data
-bitcoin_prices_df['month'] = bitcoin_prices_df['month'].map(month_map)
-
-
-
-# Group by month and calculate mean of low, high, close, and open
-monthly_data = bitcoin_prices_df.groupby('month').agg({'low': 'mean', 'high': 'mean', 'close': 'mean', 'open':'mean'}).reset_index()
+monthly_data['month'] = monthly_data['month'].map(month_map)
 
 # Dropdown for the dataframe
 with st.expander("Data Preview"):
@@ -121,44 +120,47 @@ month = st.sidebar.multiselect("Pick your Month", bitcoin_prices_df["month"].uni
 filtered_df = bitcoin_prices_df[bitcoin_prices_df["month"].isin(month)] if month else bitcoin_prices_df.copy()
 filtered_monthly_data = monthly_data[monthly_data["month"].isin(month_map[m] for m in month)] if month else monthly_data.copy()
 
+# Create a row to display all plots together
+row1, row2 = st.columns((2))
 
 # Daily Prices Line Chart
-with col1:
-    fig1 = px.line(filtered_df, x='date', y=['low', 'high', 'close', 'open'], labels={
+with row1:
+    st.subheader('Daily Bitcoin Prices')
+    fig1 = px.line(bitcoin_prices_df, x='date', y=['low', 'high', 'close', 'open'], labels={
         'value': 'Price',
         'date': 'Date'
-    }, title='Daily Bitcoin Prices')
+    }, title='Daily Prices')
     fig1.update_layout(
         plot_bgcolor='rgba(0,0,0,0)',
         paper_bgcolor='rgba(0,0,0,0)',
-        title_font=dict(color='#00008b', size=24)
+        title_font=dict(color='orange', size=24)
     )
     st.plotly_chart(fig1, use_container_width=True)
+
+# Monthly Prices Bar Chart
+with row2:
+    st.subheader('Monthly Average Prices')
+    fig2 = px.bar(filtered_monthly_data, x='month', y=['low', 'high', 'close', 'open'], barmode='group')
+    fig2.update_layout(
+        xaxis_title='Month',
+        yaxis_title='Average Price',
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        title_font=dict(color='orange', size=24)
+    )
+    st.plotly_chart(fig2, use_container_width=True)
 
 # Generate a discrete color sequence by sampling the 'Sunset' color scale
 sunset_colors = sample_colorscale(px.colors.sequential.Sunset, [i/11 for i in range(12)])
 
 # Pie Chart
 with col2:
-    fig3 = px.pie(filtered_df, values="volume", names="month", hole=0.5, color_discrete_sequence=sunset_colors, title='Monthly Volume')
+    st.subheader('Volume wise Bitcoin')
+    fig3 = px.pie(filtered_df, values="volume", names="month", hole=0.5, color_discrete_sequence=sunset_colors)
     fig3.update_layout(
         plot_bgcolor='rgba(0,0,0,0)',
         paper_bgcolor='rgba(0,0,0,0)',
-        title_font=dict(color='#00008b', size=24)
+        title_font=dict(color='orange', size=24)
     )
     fig3.update_traces(text=filtered_df["month"], textposition="outside")  # Update text after creating fig3
     st.plotly_chart(fig3, use_container_width=True)
-
-# Monthly Prices Bar Chart
-with col2:
-    fig2 = px.bar(filtered_monthly_data, x='month', y=['low', 'high', 'close', 'open'], barmode='group', title='Monthly Average Prices')
-    fig2.update_layout(
-        xaxis_title='Month',
-        yaxis_title='Average Price',
-        plot_bgcolor='rgba(0,0,0,0)',
-        paper_bgcolor='rgba(0,0,0,0)',
-        title_font=dict(color='#00008b', size=24)
-    )
-    st.plotly_chart(fig2, use_container_width=True)
-
-
