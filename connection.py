334888252ed5bbd54087ff28f32acc7c340a8c3d
gabriela-arlt <gabriela.arlt.mail@gmail.com>
2024-06-12 08:27:21 +0200

    color

--- connection.py
+++ connection.py
@@ -136,11 +136,11 @@ with row2:
 # Filter data based on selected month(s)
 filtered_df = bitcoin_prices_df[bitcoin_prices_df["month"].isin(month)] if month else bitcoin_prices_df.copy()
 
-
+#color_discrete_sequence=px.colors.qualitative.Plasma
 # Pie Chart
 with col2:
     st.subheader('Volume wise Bitcoin')
-    fig3 = px.pie(filtered_df, values="volume", names="month", hole=0.5, color_discrete_sequence=px.colors.qualitative.Pastel)
+    fig3 = px.pie(filtered_df, values="volume", names="month", hole=0.5,  color_discrete_sequence=px.colors.sequential.mygbm)
     fig3.update_traces(text=filtered_df["month"], textposition="outside")  # Update text after creating fig3
     st.plotly_chart(fig3, use_container_width=True)
 
